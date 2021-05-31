0f58fb6e22e74cf982ef495caed9433eb4898005
Ansh Godha <ag759@cornell.edu>
2021-05-31 01:24:18 -0400

    Initial commit with basic functions

---
+++ student/helpers.js
@@ -0,0 +1,21 @@
+const { db } = require("../config");
+const studentRef = db.collection("students");
+
+async function assertIsNewStudentHelper(email) {
+  const snapshot = await studentRef.where("email", "==", email).get();
+  return !snapshot.docs.length;
+}
+
+async function assertIsNewStudent(email) {
+  if (!(await assertIsNewStudentHelper(email)))
+    throw new Error("This student already exists");
+}
+
+async function assertIsExistingStudent(email) {
+  if (await assertIsNewStudentHelper(email))
+    throw new Error("This student doesn't exist");
+}
+
+function validateEmail(emailString) {}
+
+module.exports = { assertIsNewStudent, assertIsExistingStudent };
---
+++ student/routes.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const {
+  addStudent,
+  deleteStudent,
+  updateStudentInformation,
+} = require("./functions");
+const router = express.Router();
+
+router.post("/add", (req, res) => {
+  const { name, email, modality, timezone } = req.body;
+  addStudent(name, email, modality, timezone)
+    .then(() =>
+      res.status(200).json({
+        success: true,
+      })
+    )
+    .catch((err) =>
+      res.status(400).json({
+        success: false,
+        err: err.message,
+      })
+    );
+});
+
+router.delete("/delete/:email", (req, res) => {
+  const email = req.params.email;
+
+  deleteStudent(email)
+    .then(() =>
+      res.status(200).json({
+        success: true,
+      })
+    )
+    .catch((err) =>
+      res.status(400).json({
+        success: false,
+        err: err.message,
+      })
+    );
+});
+
+router.post("/update", (req, res) => {
+  const { email, modality, timezone } = req.body;
+
+  updateStudentInformation(email, modality, timezone)
+    .then(() =>
+      res.status(200).json({
+        success: true,
+      })
+    )
+    .catch((err) =>
+      res.status(400).json({
+        success: false,
+        err: err.message,
+      })
+    );
+});
+
+module.exports = router;
