b9e8e1e2fc9b22c8dd7a7ca50d2d083f7a267183
Changyuan Lin <coreballium@outlook.com>
2021-06-22 20:47:42 -0600

    New survey format: state, components, submit

--- frontend/src/modules/Survey/Components/Survey.tsx
+++ frontend/src/modules/Survey/Components/Survey.tsx
@@ -4,15 +4,11 @@ import { StyledContainer1, StyledContainer2 } from 'Survey/Styles/Survey.style'
 import { StepTemplate } from 'Survey/Components/StepTemplate'
 import { StepBegin } from 'Survey/Components/StepBegin'
 import { StepCourse } from 'Survey/Components/StepCourse'
-import { StepYear } from 'Survey/Components/StepYear'
 import { StepRadio } from 'Survey/Components/StepRadio'
 import { StepFinal } from 'Survey/Components/StepFinal'
 import {
-  getLetter,
-  getOldestGradYear,
-  getYoungestGradYear,
   sendSurveyData,
-  surveyData,
+  SurveyData,
 } from 'Survey/Components/FuncsAndConsts/SurveyFunctions'
 import { Question } from 'Survey/Types/Questions'
 
@@ -21,44 +17,38 @@ export const Survey = () => {
   const [currStep, setCurrStep] = useState(0)
   // If there are custom questions the below will be a network call perhaps
   const questions: Question[] = require('./FuncsAndConsts/Questions.json')
-  const totalSteps = questions.length + 1 // One special question
+  const numSpecialQuestions = 1 // Course list
+  const totalSteps = questions.length + numSpecialQuestions
 
   // Form answer props
   const [nameAnswer, setNameAnswer] = useState('')
   const [emailAnswer, setEmailAnswer] = useState('')
   const [courseList, setCourseList] = useState<string[]>([])
-  const [gradAnswer, setGradAnswer] = useState('')
-  const [collegeAnswer, setCollegeAnswer] = useState('')
-  const [locationAnswer, setLocationAnswer] = useState('')
-  const [groupPrefAnswer, setGroupPrefAnswer] = useState('')
-  const [studyTimeAnswer, setStudyTimeAnswer] = useState('')
+  const [answers, setAnswers] = useState(Array<string>(questions.length).fill('')) // Will be in order of Qs
+
+  const changeAnswer = (i: number, v: string) => {
+    setAnswers(answers.map((value, index) => index === i ? v : value))
+  }
 
   // last step's Next button handles sending data
   function finalNext() {
-    const surveyDataObj: surveyData = {
+    const mcData = Object.fromEntries(questions.map((question, index) => [question.questionId, answers[index]]))
+    const surveyData: SurveyData = {
       courseIds: courseList,
       fullName: nameAnswer,
-      studentId: 'a1', // ??? TODO ???
-      graduation: getLetter(gradAnswer, true),
-      college: getLetter(collegeAnswer, false),
-      remote: getLetter(locationAnswer, false),
-      mode: getLetter(groupPrefAnswer, false),
-      time: getLetter(studyTimeAnswer, false),
+      email: emailAnswer,
+      ...mcData
     }
-    sendSurveyData(surveyDataObj)
+    console.log('Finished survey', surveyData)
+    sendSurveyData(surveyData)
     setCurrStep(currStep + 1)
   }
 
+  const multipleChoiceIndex = currStep - numSpecialQuestions - 1
   const isStepValid =
-    currStep === 1 ? courseList.length > 0 :
-    currStep === 2 ? collegeAnswer !== '' :
-    currStep === 3 ? Number(gradAnswer) <= getYoungestGradYear()
-                     && Number(gradAnswer) >= getOldestGradYear() :
-    currStep === 4 ? locationAnswer !== '' :
-    currStep === 5 ? groupPrefAnswer !== '' :
-    currStep === 6 ? studyTimeAnswer !== '' : true
+    currStep === 1 ? courseList.length > 0 : answers[multipleChoiceIndex] !== ''
 
-  return currStep === 0 ? (
+  return currStep === 0 ? ( // Form landing
     <StyledContainer1>
       <StepBegin
         name={nameAnswer}
@@ -68,15 +58,9 @@ export const Survey = () => {
         gotoNextStep={() => setCurrStep((currStep) => currStep + 1)}
       />
     </StyledContainer1>
-  ) : currStep === 7 ? (
+  ) : currStep === totalSteps + 1 ? ( // Form confirmation
     <StyledContainer2>
       <StepFinal />
-      {/* FOR DEBUGGING NETWORKING + ANSWERS */}
-      <p>{getLetter(gradAnswer, true)}</p>
-      <p>{getLetter(collegeAnswer, false)}</p>
-      <p>{getLetter(locationAnswer, false)}</p>
-      <p>{getLetter(groupPrefAnswer, false)}</p>
-      <p>{getLetter(studyTimeAnswer, false)}</p>
     </StyledContainer2>
   ) : (
     <StyledContainer2>
@@ -88,51 +72,20 @@ export const Survey = () => {
         gotoPrevStep={() => setCurrStep((currStep) => currStep - 1)}
         gotoNextStep={currStep === totalSteps ? finalNext : () => setCurrStep((currStep) => currStep + 1)}
       >
-        {currStep === 1 ? (
+        {currStep === 1 ? ( // Course selection
           <StepCourse
             courses={courseList}
             setCourses={setCourseList}
           />
-        ) : currStep === 2 ? (
+        ) : ( // General multiple-choice
           <StepRadio
             showError={showError}
-            currentAnswer={collegeAnswer}
-            question={questions[0]}
-            setAnswer={setCollegeAnswer}
+            currentAnswer={answers[multipleChoiceIndex]}
+            question={questions[multipleChoiceIndex]}
+            setAnswer={(arg) => changeAnswer(multipleChoiceIndex, arg)}
             key={String(currStep)}
           />
-        ) : currStep === 3 ? (
-          // <StepYear
-          //   showError={showError}
-          //   currentAnswer={gradAnswer}
-          //   setAnswer={setGradAnswer}
-          // />
-          null
-        ) : currStep === 4 ? (
-          <StepRadio
-            showError={showError}
-            currentAnswer={locationAnswer}
-            question={questions[1]}
-            setAnswer={setLocationAnswer}
-            key={String(currStep)}
-          />
-        ) : currStep === 5 ? (
-          <StepRadio
-            showError={showError}
-            currentAnswer={groupPrefAnswer}
-            question={questions[2]}
-            setAnswer={setGroupPrefAnswer}
-            key={String(currStep)}
-          />
-        ) : currStep === 6 ? (
-          <StepRadio
-            showError={showError}
-            currentAnswer={studyTimeAnswer}
-            question={questions[3]}
-            setAnswer={setStudyTimeAnswer}
-            key={String(currStep)}
-          />
-        ) : null}
+        )}
       </StepTemplate>
     </StyledContainer2>
   )
