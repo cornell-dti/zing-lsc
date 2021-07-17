f83128769cf59fdf0cde0788fc88e48c7194a950
Ansh Godha <ag759@cornell.edu>
2021-07-17 17:03:57 -0400

    Add endpoint for student data

--- backend/functions/course/functions.js
+++ backend/functions/course/functions.js
@@ -1,5 +1,19 @@
+require("dotenv").config({ path: "../.env" });
 const { db } = require("../config");
-const courseRef = db.collection("courses");
+const courseRef = db.collection("courses_test");
+const studentRef = db.collection("students_test");
+
+async function getStudent(email) {
+  const snapshot = await studentRef.doc(email).get();
+  if (!snapshot.exists) throw new Error(`Student ${email} does not exist`);
+  const result = snapshot.data();
+  result.email = email;
+  return result;
+}
+
+async function getDataForStudents(emails) {
+  return Promise.all(emails.map((email) => getStudent(email)));
+}
 
 async function getCourseInfo(courseId) {
   const snapshot = await courseRef.doc(courseId).get();
@@ -16,4 +30,32 @@ async function getAllCourses() {
   });
 }
 
-module.exports = { getCourseInfo, getAllCourses };
+async function getStudentsForCourse(courseId) {
+  const courseSnapshot = await courseRef.doc(courseId).get();
+  if (!courseSnapshot.exists)
+    throw new Error(`Course ${courseId} does not exist`);
+  const courseData = courseSnapshot.data();
+  const unmatched = courseData.unmatched;
+
+  const groupsQueryDocSnapshots = (
+    await courseSnapshot.ref.collection("groups").get()
+  ).docs;
+
+  const data = groupsQueryDocSnapshots.map((snapshot) => snapshot.data());
+  const unmatchedStudentData = await getDataForStudents(unmatched);
+  const groupStudentDataRaw = await Promise.all(
+    data.map((group) => getDataForStudents(group.members))
+  );
+
+  const groupStudentData = groupStudentDataRaw.map((groupData, index) => ({
+    memberData: groupData,
+    groupNumber: data[index].groupNumber,
+  }));
+
+  return {
+    unmatched: unmatchedStudentData,
+    groups: groupStudentData,
+  };
+}
+
+module.exports = { getCourseInfo, getAllCourses, getStudentsForCourse };
