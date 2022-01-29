adb7289152188b38369d11a3c5fac317fda59d5a
mluo24 <miranda.luo24@gmail.com>
2022-01-29 14:06:54 -0500

    finished appearance of input components

--- frontend/src/modules/Core/Components/PageNumbers.tsx
+++ frontend/src/modules/Core/Components/PageNumbers.tsx
@@ -5,18 +5,23 @@ import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos'
 const PageNumberButton = ({ direction }: { direction: string }) => {
   return (
     <Box
-      sx={{ fontFamily: 'Montserrat', fontWeight: 600 }}
+      sx={{
+        fontFamily: 'Montserrat',
+        fontWeight: 600,
+        color: '#000',
+        '& .arrow-icon': { color: '#898992' },
+      }}
       display="flex"
       alignItems="center"
       gap={1}
     >
       {direction === 'left' ? (
         <>
-          <ArrowBackIosNewIcon /> Back
+          <ArrowBackIosNewIcon className="arrow-icon" /> Back
         </>
       ) : (
         <>
-          Next <ArrowForwardIosIcon />
+          Next <ArrowForwardIosIcon className="arrow-icon" />
         </>
       )}
     </Box>
@@ -38,6 +43,15 @@ const PageNumbers = ({ count }: { count: number }) => {
       count={count}
       renderItem={(item) => (
         <PaginationItem
+          sx={{
+            fontFamily: 'Montserrat',
+            color: '#898992',
+            fontWeight: 600,
+            '&.Mui-selected': {
+              backgroundColor: '#DBDBDD',
+              color: 'black',
+            },
+          }}
           components={{
             previous: BackButton,
             next: NextButton,
