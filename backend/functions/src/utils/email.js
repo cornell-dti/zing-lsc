690625864474ea1028246bc920f3f96c4a628977
Will Zhang <wz282@cornell.edu>
2022-03-11 00:44:59 -0500

    Email feature works in testing suite with templates
    - Ethereal testing suite
    - Still need to be able to use LSC email though
    - Subject line in switch case statements?

--- backend/functions/src/utils/email.js
+++ backend/functions/src/utils/email.js
@@ -1,7 +1,7 @@
 const nodemailer = require('nodemailer')
 const express = require('express')
 const router = express.Router()
-import { matched } from './templates/Matched'
+import * as emails from './templates/Matched'
 
 var transport = {
   host: 'smtp.ethereal.email',
@@ -28,21 +28,75 @@ router.post('/test', (req, res, next) => {
   res.json('hello')
 })
 
-const localMessage = `<div> Dear students, <div> my local message 1 </div>
-  <div> Best, the LSC. </div></div>`
+function parseRecipients(recipients) {
+  let res = ''
+  recipients.forEach((e) => {
+    res += ', ' + e
+  })
+  return res
+}
 
 // Route for sending an email
 router.post('/send', (req, res, next) => {
-  const name = req.body.name
-  const email = req.body.email
-  const message = req.body.messageHtml
+  const {
+    name,
+    email,
+    recipientsList,
+    message,
+    template,
+    newStudent,
+    existingStudents,
+    className,
+  } = req.body
+
+  const recipients = parseRecipients(recipientsList)
+  //  [body] can be of type email.
+  // {
+  //   matched, firstNoMatch, secondNoMatch, addStudent,
+  //   lateAddStudent, askJoinGroup, checkIn
+  // }
+  let body
+  switch (template) {
+    case 'matched': {
+      body = emails.matched()
+      break
+    }
+    case 'firstNoMatch': {
+      body = emails.firstNoMatch(className)
+      break
+    }
+    case 'secondNoMatch': {
+      body = emails.secondNoMatch()
+      break
+    }
+    case 'addStudent': {
+      body = emails.addStudent()
+      break
+    }
+    case 'lateAddStudent': {
+      body = emails.lateAddStudent()
+      break
+    }
+    case 'askJoinGroup': {
+      body = emails.askJoinGroup(newStudent, existingStudents)
+      break
+    }
+    case 'checkIn': {
+      body = emails.checkIn()
+      break
+    }
+    default: {
+      body = 'LSC did not select a valid email template.'
+      break
+    }
+  }
 
   let mail = {
     from: 'zhanliam21@gmail.com',
     to: `wi37u2zcwxyqd7nu@ethereal.email, retta.keebler47@ethereal.email`,
-    cc: ``,
+    cc: `${recipients}`,
     subject: `Cornell LSC Student Matching Results`,
-    html: matched,
+    html: body,
   }
 
   transporter.sendMail(mail, (err, data) => {
