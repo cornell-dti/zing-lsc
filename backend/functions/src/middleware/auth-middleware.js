5fbc8a7fab0adb4fe5884caa96879ecf766833c2
mluo24 <miranda.luo24@gmail.com>
2022-03-20 16:52:42 -0400

    finished the checkAuth function

--- backend/functions/src/middleware/auth-middleware.js
+++ backend/functions/src/middleware/auth-middleware.js
@@ -1,9 +1,19 @@
-let authorized = false
+import * as admin from 'firebase-admin'
 
+// function courtesy of https://itnext.io/how-to-use-firebase-auth-with-a-custom-node-backend-99a106376c8a
+// checks if valid based on the auth token in firebase admin
 export function checkAuth(req, res, next) {
-  if (authorized) {
-    next()
+  if (req.headers.authtoken) {
+    admin
+      .auth()
+      .verifyIdToken(req.headers.authtoken)
+      .then(() => {
+        next()
+      })
+      .catch(() => {
+        res.status(403).send('Unauthorized: error occurred')
+      })
   } else {
-    res.status(403).send('Unauthorized')
+    res.status(403).send('Unauthorized: please sign in')
   }
 }
