69ce355ea7e1166137f59fcda173c42a5ccbba02
Will Zhang <wz282@cornell.edu>
2022-03-30 18:53:50 -0400

    Made everything into typescript for the backend

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,3 +1,13 @@
+import * as functions from 'firebase-functions'
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
 import express from 'express'
 import { config } from 'dotenv'
 config()
