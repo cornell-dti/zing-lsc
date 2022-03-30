094af5b315c755a1b0c4173d34ea1aeb9a4fde78
Will Zhang <wz282@cornell.edu>
2022-03-30 18:43:07 -0400

    before switch

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,25 +1,15 @@
-import * as functions from 'firebase-functions'
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
-const express = require('express')
-require('dotenv').config()
-
+import express from 'express'
+import { config } from 'dotenv'
+config()
 // Initialize express app
 const app = express()
+
 app.use(express.json())
 
 // import routers
-const studentsRouter = require('./student/routes')
-const matchingRouter = require('./matching/routes')
-const courseRouter = require('./course/routes')
-// const { makeMatches } = require("./matching/functions");
+import studentsRouter from './student/routes'
+import matchingRouter from './matching/routes'
+import courseRouter from './course/routes'
 app.use('/student', studentsRouter)
 app.use('/matching', matchingRouter)
 app.use('/course', courseRouter)
