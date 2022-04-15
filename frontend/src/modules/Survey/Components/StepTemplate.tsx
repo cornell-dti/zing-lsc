ad2a60f47bb503cbefcb4ff19f58267ab9030974
CollinWoo <collinwu01@gmail.com>
2022-04-15 18:28:28 -0400

    add new prev and next buttons

--- frontend/src/modules/Survey/Components/StepTemplate.tsx
+++ frontend/src/modules/Survey/Components/StepTemplate.tsx
@@ -10,7 +10,9 @@ import {
 } from 'Survey/Styles/StepTemplate.style'
 import prev from '@assets/img/prev.svg'
 import next from '@assets/img/next.svg'
-import { ProgressBar } from '@core/Components/ProgressBar'
+import { ProgressBar } from '@core/Components/index'
+import { IconButton, Box } from '@mui/material'
+import { ArrowBack, ArrowForward } from '@mui/icons-material'
 
 export const StepTemplate: FunctionComponent<StepTemplateProps> = ({
   isStepValid,
@@ -36,25 +38,25 @@ export const StepTemplate: FunctionComponent<StepTemplateProps> = ({
   }
 
   return (
-    <StyledContainer>
-      <StyledFullPanelNoPadding>
-        <ProgressBar step={stepNumber} total={totalSteps} />
-        <StyledFullPanel>
-          <StyledWrapper style={{ height: '90%' }}>{children}</StyledWrapper>
-          <StyledWrapper style={{ height: '10%' }}>
-            <StyledPrevButton
-              className="prev"
-              src={prev}
-              onClick={handlePrev}
-            />
-            <StyledNextButton
-              className="next"
-              src={next}
-              onClick={handleNext}
-            />
-          </StyledWrapper>
-        </StyledFullPanel>
-      </StyledFullPanelNoPadding>
-    </StyledContainer>
+    <Box sx={{ backgroundColor: 'white' }}>
+      <ProgressBar step={stepNumber} total={totalSteps} />
+      <StyledFullPanel>
+        <StyledWrapper style={{ height: '90%', display: 'flex' }}>
+          {children}
+        </StyledWrapper>
+        <StyledWrapper style={{ height: '10%' }}>
+          <IconButton className="prev" onClick={handlePrev}>
+            <ArrowBack />
+          </IconButton>
+          <IconButton
+            className="next"
+            onClick={handleNext}
+            sx={{ marginLeft: 'auto' }}
+          >
+            {<ArrowForward />}
+          </IconButton>
+        </StyledWrapper>
+      </StyledFullPanel>
+    </Box>
   )
 }
