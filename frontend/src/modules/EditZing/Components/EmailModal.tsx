a80a815d40aa36d6005ce9dbedab25507796dcd8
Will Zhang <wz282@cornell.edu>
2022-06-05 20:49:03 -0400

    fix so separate emails send for each group and next steps

--- frontend/src/modules/EditZing/Components/EmailModal.tsx
+++ frontend/src/modules/EditZing/Components/EmailModal.tsx
@@ -1,11 +1,15 @@
+// external imports
+import { useState } from 'react'
+import { Box, Button, Typography } from '@mui/material'
+import SendIcon from '@mui/icons-material/Send'
+// zing imports
+import { getBody } from '../utils/emailTemplates'
 import { ZingModal } from '@core/Components'
 import { EmailModalProps } from '../Types/ComponentProps'
-import { useState } from 'react'
 import { TemplateName } from 'EditZing/utils/emailTemplates'
-import { Box, Button, Typography } from '@mui/material'
 import { EmailTemplateButtons } from 'EditZing/Components/EmailTemplateButtons'
 import { EmailPreview } from 'EditZing/Components/EmailPreview'
-import SendIcon from '@mui/icons-material/Send'
+import { sendEmail } from 'Emailing/Components/Emailing'
 
 export const EmailModal = ({
   selectedGroups,
@@ -19,6 +23,38 @@ export const EmailModal = ({
   const [step, setStep] = useState<number>(0)
   const titles = ['Select email template', 'Send emails']
   const title = titles[step]
+  const [sendError, setSendError] = useState<boolean | null>(null)
+
+  // ======= Send Email Helper Functions =======
+  const sendEmails = async () => {
+    selectedGroups.forEach(async (group) => {
+      const rcpts: string[] = []
+      const members = group.memberData
+      members.forEach((mem) => {
+        rcpts.push(mem.email)
+      })
+      const emailBody = getBody(selectedTemplate, courseNames.join(', '))
+      const emailSubject = 'Study Partners!'
+      const emailItems = { emailSubject, rcpts, emailBody }
+      await sendEmail(emailItems).then((res) => {
+        console.log(`email send return is: ${res}`)
+        if (res === false) {
+          setSendError(true)
+        }
+      })
+    })
+    sendError ? setStep(2) : setStep(3)
+  }
+
+  // const sendMail = async () => {
+  //   // console.log(emailItems())
+  //   // await sendEmail(emailItems()).then((res) => {
+  //   //   console.log(`email send return is: ${res}`)
+  //   //   if (res) {
+  //   //     setStep(2)
+  //   //   } else setStep(3)
+  //   // })
+  // }
 
   const TemplateSelectedComponent = () => {
     return (
@@ -56,6 +92,69 @@ export const EmailModal = ({
     )
   }
 
+  /** Step 2
+   *  Show email sent screen
+   *  Close Button */
+  const StepSent = () => {
+    return (
+      <Box>
+        <Typography variant="h4" component="h4" fontWeight={'700'}>
+          Email Sent! All done.
+        </Typography>
+      </Box>
+    )
+  }
+
+  /** Step 3
+   *  Show email send failure screen
+   *  Try Again Button */
+  const StepFailure = () => {
+    return (
+      <Box>
+        <Typography variant="h4" component="h4" fontWeight={'700'}>
+          Uh oh... Something went wrong when trying to send the email. Try to
+          sign in one more time.{' '}
+          <em> Will automatically attempt to send the email one more time. </em>
+        </Typography>
+        <Button
+          onClick={() => {
+            // adminSignIn().then(() => sendEmail())
+            /**
+             * TODO import adminSignIn and implement try again button
+             * TODO set next to next screen to either : sent YAY or : Failed again. Please attempt emailing manually. Sorry for the inconvience.
+             * */
+          }}
+        >
+          Try Again{' '}
+        </Button>
+      </Box>
+    )
+  }
+
+  /** Step 4
+   *  Show email send failure screen
+   *  Try Again Button */
+  const StepFinalFailure = () => {
+    return (
+      <Box>
+        <Typography variant="h4" component="h4" fontWeight={'700'}>
+          Failed again. Please attempt emailing manually. Sorry for the
+          inconvience.
+        </Typography>
+        <Button
+          onClick={() => {
+            // adminSignIn().then(() => sendEmail())
+            /**
+             * TODO close button
+             * */
+          }}
+        >
+          Close{' '}
+        </Button>
+      </Box>
+    )
+  }
+
   const ProceedButton = () => {
     if (step === 0) {
       return (
@@ -73,7 +172,9 @@ export const EmailModal = ({
       return (
         <Button
           onClick={() => {
-            setIsEmailing(!isEmailing)
+            // setIsEmailing(!isEmailing)
+            sendEmails()
+            console.log('sending email')
             // add something else here that actually sends the emails and a pop-up message saying that it's in-progress then done/fail etc.
           }}
           endIcon={<SendIcon />}
@@ -130,7 +231,11 @@ export const EmailModal = ({
                 .join(', ')}
             </Box>
           </Typography>
-          {step === 0 ? <Step0 /> : <Step1 />}
+          {step === 0 && <Step0 />}
+          {step === 1 && <Step1 />}
+          {step === 2 && <StepSent />}
+          {step === 3 && <StepFailure />}
+          {step === 4 && <StepFinalFailure />}
         </Box>
       </ZingModal.Body>
       <ZingModal.Controls>
