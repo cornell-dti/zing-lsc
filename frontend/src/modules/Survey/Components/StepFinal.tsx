08c15919dc781ca5cb47fe11b58039a29907e5ea
Changyuan Lin <coreballium@outlook.com>
2022-06-22 23:24:34 -0700

    Partial survey success on frontend

--- frontend/src/modules/Survey/Components/StepFinal.tsx
+++ frontend/src/modules/Survey/Components/StepFinal.tsx
@@ -10,19 +10,43 @@ import {
 } from 'Survey/Styles/StepFinal.style'
 import { StepFinalProps } from 'Survey/Types'
 import check from '@assets/img/whitecheckmark.svg'
+import warn from '@assets/img/warnbutton.svg'
 import xmark from '@assets/img/xbutton.svg'
 
-export const StepFinal = ({ success, errorMsg }: StepFinalProps) => {
+export const StepFinal = ({
+  success,
+  submissionResponse,
+  errorMsg,
+}: StepFinalProps) => {
+  const showWarning = success && submissionResponse.failed.length !== 0
+
   return (
     <StyledContainer>
       <StyledFullPanel>
         <StyledLogoWrapper>
           <StyledCongratulationsWrapper>
-            <StyledCheck src={success ? check : xmark} />
-            {success ? (
+            <StyledCheck src={showWarning ? warn : success ? check : xmark} />
+            {showWarning ? (
+              <StyledCongratulationsText>
+                Some course(s) could not be found in roster{' '}
+                {submissionResponse.roster}. Please double-check that they were
+                spelled correctly, and resubmit the form if needed.
+                <br />
+                <StyledContactText>
+                  Courses added successfully:{' '}
+                  {submissionResponse.added.join(', ') || 'none'}
+                  <br />
+                  Courses not added: {submissionResponse.failed.join(', ')}
+                </StyledContactText>
+              </StyledCongratulationsText>
+            ) : success ? (
               <StyledCongratulationsText>
                 Congratulations on completing the form! You should receive an
                 email with your team members shortly.
+                <br />
+                <StyledContactText>
+                  Courses added: {submissionResponse.added.join(', ')}
+                </StyledContactText>
               </StyledCongratulationsText>
             ) : (
               <StyledCongratulationsText>
