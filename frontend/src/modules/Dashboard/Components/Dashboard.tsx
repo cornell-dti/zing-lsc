112663c73f8613fb3da2302d43752e3c7d9608ce
Benjamin Tang <bt283@cornell.edu>
2022-10-13 18:59:54 -0400

    fixed lint errors

--- frontend/src/modules/Dashboard/Components/Dashboard.tsx
+++ frontend/src/modules/Dashboard/Components/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useState } from 'react'
+import React, { useState } from 'react'
 import Button from '@mui/material/Button'
 import Menu from '@mui/material/Menu'
 import MenuItem from '@mui/material/MenuItem'
@@ -197,13 +197,14 @@ export const Dashboard = () => {
     console.log('search is:', event.target.value)
   }
 
-  const filteredCourses = courses
-    .filter((c) => c.roster === selectedRoster)
-    .filter((d) =>
-      d.names.find((e) => {
-        return e.includes(message.toUpperCase())
-      })
-    )
+  const filteredCourses = sorted(
+    courses.filter((c) => c.roster === selectedRoster),
+    sortedOrder
+  ).filter((d) =>
+    d.names.find((e) => {
+      return e.includes(message.toUpperCase())
+    })
+  )
 
   return (
     <StyledContainer>
