41530bb63d9452423e2ad4a3abf597902c300428
mluo24 <miranda.luo24@gmail.com>
2022-11-09 17:38:25 -0500

    add back appcheck

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -42,12 +42,13 @@ import './App.css'
 import theme from '@core/Constants/Theme'
 import { User, onAuthStateChanged } from 'firebase/auth'
 import { AuthProvider, AuthState, PrivateRoute, PublicRoute } from '@auth'
-import { auth } from '@fire'
+import { appCheck, auth } from '@fire'
 import { templatesBucket } from '@fire/firebase'
 import { getDownloadURL, ref } from 'firebase/storage'
 import axios, { AxiosResponse } from 'axios'
 import { CourseProvider, StudentProvider } from '@context'
 import { TemplateProvider } from '@context/TemplateContext'
+import { getToken } from 'firebase/app-check'
 
 const App = () => {
   const [currentUser, setCurrentUser] = useState<User | null>(null)
@@ -102,6 +103,21 @@ const App = () => {
       }
     })
   }, [])
+
+  // Because we are calling backend through HTTP directly, it's like a custom resource:
+  // https://firebase.google.com/docs/app-check/web/custom-resource
+  useEffect(() => {
+    axios.interceptors.request.use(
+      async (request) => {
+        const appCheckTokenResponse = await getToken(appCheck, false)
+        request.headers['X-Firebase-AppCheck'] = appCheckTokenResponse.token
+        return request
+      },
+      (error) => {
+        return Promise.reject(error)
+      }
+    )
+  })
 
   // Application-wide courses are only loaded when user is authorized
   const [hasLoadedCourses, setHasLoadedCourses] = useState(false)
