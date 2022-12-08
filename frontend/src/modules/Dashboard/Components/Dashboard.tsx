509d8c282da9187e9c93d901cf9c827823db3127
rgu0114 <richardgu0114@gmail.com>
2022-12-08 17:18:13 -0500

    Created add to existing group filter

--- frontend/src/modules/Dashboard/Components/Dashboard.tsx
+++ frontend/src/modules/Dashboard/Components/Dashboard.tsx
@@ -21,6 +21,7 @@ type FilterOption =
   | 'unmatchable'
   | 'newly-matchable'
   | 'matchable'
+  | 'can-add-to-existing-group'
   | 'no-check-in-email'
   | 'no-no-match-email'
 
@@ -32,6 +33,7 @@ const filterOptionDisplay = [
   ['unmatchable', 'Unmatchable'],
   ['newly-matchable', 'Newly Matchable'],
   ['matchable', 'Matchable'],
+  ['can-add-to-existing-group', 'Can Add to Existing group'],
   ['no-check-in-email', 'No Check In Email'],
   ['no-no-match-email', 'No No Match Email'],
 ]
@@ -98,6 +100,11 @@ export const Dashboard = () => {
           (course, _) =>
             (course.lastGroupNumber > 0 && course.unmatched.length > 0) ||
             (course.lastGroupNumber === 0 && course.unmatched.length > 1)
+        )
+      case 'can-add-to-existing-group':
+        return [...courseInfo].filter(
+          (course, _) =>
+            course.lastGroupNumber > 0 && course.unmatched.length === 1
         )
       case 'no-check-in-email':
         return courseInfo.filter(hasUnsentCheckIns)
