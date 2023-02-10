da1c6755e75e86675971ad8dc5321cf6ee619061
Justin Kong <jk2338@cornell.edu>
2023-02-10 17:00:16 -0500

    frontend for settings page to change semesters

--- frontend/src/modules/Settings/Components/Settings.tsx
+++ frontend/src/modules/Settings/Components/Settings.tsx
@@ -3,22 +3,62 @@ import { Box, IconButton, SelectChangeEvent } from '@mui/material'
 import Menu from '@mui/material'
 import MenuItem from '@mui/material/MenuItem'
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { DASHBOARD_PATH } from '@core/index'
+import { ReactComponent as LogoImg } from '@assets/img/lscicon.svg'
+import { AccountMenu } from 'Dashboard/Components/AccountMenu'
 
 export const Settings = () => {
   const [currRoster, setCurrRoster] = useState<string>('SP23')
 
   const changeCurrRoster = (event: SelectChangeEvent) => {
+    // function to only open the survey of the semester
     setCurrRoster(event.target.value)
   }
   return (
-    <Box>
+    <Box
+      sx={{ pl: '5rem', pr: '5rem', display: 'flex', flexDirection: 'column' }}
+    >
+      <Box
+        sx={{
+          width: '100%',
+          height: 'fit-content',
+          padding: '2.5rem',
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }}
+      >
+        <IconButton
+          color="secondary"
+          component={Link}
+          to={{
+            pathname: DASHBOARD_PATH,
+          }}
+          sx={{
+            border: 'none',
+            alignSelf: 'flex-start',
+          }}
+          disableRipple
+          disableFocusRipple
+        >
+          <LogoImg />
+        </IconButton>
+        <AccountMenu
+          selectedRoster={currRoster}
+          setSelectedRoster={setCurrRoster}
+          showMetricsLink={true}
+          showDashboardLink={true}
+          showSettingsLink={false}
+        />
+      </Box>
       <Box
         sx={{
           pl: '5rem',
           pr: '5rem',
-          display: 'flex',
-          flexDirection: 'row',
-          background: 'green',
+          display: 'grid',
+          alignItems: 'center',
         }}
       >
         <Box sx={{ typography: 'h5' }}>Change Semester</Box>
@@ -26,7 +66,10 @@ export const Settings = () => {
         <DropdownSelect
           value={currRoster}
           onChange={changeCurrRoster}
-          sx={{ alignSelf: 'right', background: 'blue' }}
+          sx={{
+            alignContent: 'right',
+            right: '1px',
+          }}
         >
           <MenuItem value="SU22">Summer 2022</MenuItem>
           <MenuItem value="FA22">Fall 2022</MenuItem>
