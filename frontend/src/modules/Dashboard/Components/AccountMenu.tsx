100a622b2d6c425b36838751a721c6ad553f857f
Benjamin Tang <bt283@cornell.edu>
2023-02-23 18:35:51 -0500

    Revert "generated year month date"
    This reverts commit ad3bf5188a6e7c2117b39103cac851fc7680ca6f.

--- frontend/src/modules/Dashboard/Components/AccountMenu.tsx
+++ frontend/src/modules/Dashboard/Components/AccountMenu.tsx
@@ -87,11 +87,6 @@ export const AccountMenu = ({
     setRosterAnchorEl(null)
   }
 
-  const today = new Date()
-  const year = today.getFullYear()
-  const month = today.getMonth() + 1
-  const date = today.getDate()
-
   return (
     <Box>
       <Button
