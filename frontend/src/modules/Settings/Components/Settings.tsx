c0118ff55b5f2a4b9c7a3d5a905cd480326369e9
Justin Kong <jk2338@cornell.edu>
2023-02-23 19:11:57 -0500

    frontend opening and closing survey

--- frontend/src/modules/Settings/Components/Settings.tsx
+++ frontend/src/modules/Settings/Components/Settings.tsx
@@ -1,6 +1,5 @@
 import { DropdownSelect } from '@core/index'
-import { Box, IconButton, SelectChangeEvent } from '@mui/material'
-import Menu from '@mui/material'
+import { Box, IconButton, SelectChangeEvent, Switch } from '@mui/material'
 import MenuItem from '@mui/material/MenuItem'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
@@ -11,11 +10,18 @@ import { AccountMenu } from 'Dashboard/Components/AccountMenu'
 export const Settings = () => {
   const [currRoster, setCurrRoster] = useState<string>('SP23')
   const semesters = ['SU22', 'FA22', 'WI23', 'SP23', 'SU23']
+  const [surveyOpen, setSurveyOpen] = useState<boolean>(true)
 
   const changeCurrRoster = (event: SelectChangeEvent) => {
     // function to only open the survey of the semester
     setCurrRoster(event.target.value)
   }
+
+  const changeSurveyAvailability = (event: SelectChangeEvent) => {
+    setSurveyOpen(!surveyOpen)
+    console.log(surveyOpen)
+  }
+
   return (
     <Box
       sx={{ pl: '5rem', pr: '5rem', display: 'flex', flexDirection: 'column' }}
@@ -76,6 +82,33 @@ export const Settings = () => {
             <MenuItem value={sem}>{sem}</MenuItem>
           ))}
         </DropdownSelect>
+      </Box>
+      <Box
+        sx={{
+          width: '100%',
+          height: 'fit-content',
+          padding: '2.5rem',
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }}
+      >
+        <Box
+          sx={{
+            pl: '2.5rem',
+            pr: '2.5rem',
+            display: 'grid',
+            alignItems: 'center',
+          }}
+        >
+          <Box sx={{ typography: 'h5', pr: '2rem' }}>Open Survey</Box>
+        </Box>
+        <Switch
+          sx={{ right: '2rem' }}
+          checked={!surveyOpen}
+          onChange={changeSurveyAvailability}
+        ></Switch>
       </Box>
     </Box>
   )
