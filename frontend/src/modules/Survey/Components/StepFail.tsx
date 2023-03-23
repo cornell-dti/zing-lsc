22cfecb2800ac35d188887cbd10df72aaef6f479
Justin Kong <jk2338@cornell.edu>
2023-03-23 19:06:14 -0400

    cleaining up unnecessary code that was copied over from failure page

--- frontend/src/modules/Survey/Components/StepFail.tsx
+++ frontend/src/modules/Survey/Components/StepFail.tsx
@@ -7,78 +7,13 @@ import check from '@assets/img/whitecheckmark.svg'
 import warn from '@assets/img/warnbutton.svg'
 import xmark from '@assets/img/xbutton.svg'
 
-export const StepFail = ({
-  success,
-  submissionResponse,
-  errorMsg,
-}: StepFinalProps) => {
-  const showWarning = success && submissionResponse.failed.length !== 0
+export const StepFail = () => {
   const buttonStyle = {
     minWidth: '14em',
     fontSize: { sm: 14, md: 22 },
     mt: '1.25em',
     p: '0.24em 1em',
     boxShadow: 1,
-  }
-
-  const Success = () => {
-    return (
-      <>
-        <Typography
-          sx={{
-            fontSize: { xs: '1.5rem', md: '2.25rem' },
-            fontWeight: '700',
-            color: '#fff',
-          }}
-        >
-          Congrats on completing the form!
-        </Typography>
-        <Typography
-          sx={{
-            color: '#fff',
-            fontSize: '1.15rem',
-          }}
-        >
-          You will receive an email in the next few days with more information
-          about next steps.
-          <br />
-          <strong>Courses added: </strong>
-          {submissionResponse.added.join(', ') || `None`}
-        </Typography>
-      </>
-    )
-  }
-
-  const Warning = () => {
-    return (
-      <>
-        <Typography
-          sx={{
-            fontSize: { xs: '1.5rem', md: '2.25rem' },
-            fontWeight: '700',
-            color: '#fff',
-          }}
-        >
-          Some course(s) could not be found in roster{' '}
-          {submissionResponse.roster}.
-        </Typography>
-        <Typography
-          sx={{
-            color: '#fff',
-            fontSize: '1.15rem',
-          }}
-        >
-          Please double-check that they were spelled correctly, and resubmit the
-          form if needed.
-          <br />
-          <strong> Courses added successfully: </strong>
-          {submissionResponse.added.join(', ') || 'None'}
-          <br />
-          <strong>Courses not added: </strong>
-          {submissionResponse.failed.join(', ')}
-        </Typography>
-      </>
-    )
   }
 
   const Error = () => {
@@ -146,33 +81,18 @@ export const StepFail = ({
             alignItems: 'center',
           }}
         >
-          {showWarning && <Warning />}
-          {success && !showWarning && <Success />}
-          {!success && <Error />}
+          {<Error />}
         </Box>
       </Box>
 
-      {!showWarning && success ? (
-        <Button
-          color="secondary"
-          variant="outlined"
-          href="https://lsc.cornell.edu/"
-          sx={{ ...buttonStyle, mt: '4rem' }}
-        >
-          Back to Learning Strategies Center
-        </Button>
-      ) : (
-        <>
-          <Button
-            color="primary"
-            variant="outlined"
-            href="https://lsc.cornell.edu/"
-            sx={buttonStyle}
-          >
-            Back to Learning Strategies Center
-          </Button>
-        </>
-      )}
+      <Button
+        color="primary"
+        variant="outlined"
+        href="https://lsc.cornell.edu/"
+        sx={buttonStyle}
+      >
+        Back to Learning Strategies Center
+      </Button>
 
       <Typography
         sx={{
