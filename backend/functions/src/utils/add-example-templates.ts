83e4b5b9ddcb68fae1e22350f021d74ca2aeda4d
Benjamin Tang <bt283@cornell.edu>
2023-03-30 15:07:10 -0400

    added email to templates

--- backend/functions/src/utils/add-example-templates.ts
+++ backend/functions/src/utils/add-example-templates.ts
@@ -42,6 +42,14 @@ const templates: FirestoreEmailTemplate[] = [
     body: 'check-in.html',
     modifyTime,
   },
+  {
+    id: 'no-match-session-closed',
+    name: 'No match, session closed',
+    type: 'student',
+    subject: 'TEST Update about your study partner request',
+    body: 'no-match-session-closed.html',
+    modifyTime,
+  },
 ]
 
 const templateRef = db.collection('email_templates')
