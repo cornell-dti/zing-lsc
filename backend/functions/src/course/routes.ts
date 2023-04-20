e2b8aad2fc6cc730c6498a8d067b0c84d637bc46
rgu0114 <richardgu0114@gmail.com>
2023-04-20 19:49:38 -0400

    Fixed routes refe rencing course vs settings

--- backend/functions/src/course/routes.ts
+++ backend/functions/src/course/routes.ts
@@ -1,16 +1,7 @@
 import { Router } from 'express'
 import { config } from 'dotenv'
 import { logger } from 'firebase-functions'
-import {
-  getCourseInfo,
-  getAllCourses,
-  getStudentsForCourse,
-  getCurrentSemester,
-  setCurrentSemester,
-  getAllSemesters,
-  getSurveyStatus,
-  setSurveyStatus,
-} from './functions'
+import { getCourseInfo, getAllCourses, getStudentsForCourse } from './functions'
 
 const router = Router()
 config()
@@ -41,56 +32,6 @@ router.get('/students/:courseId', (req, res) => {
     .catch((err) => {
       console.log(err)
       res.status(400).send({ success: false, err: err.message })
-    })
-})
-
-router.get('/semester/current', (_, res) => {
-  getCurrentSemester()
-    .then((data) => res.status(200).send(data))
-    .catch((err) => {
-      const err_msg = `Unexpected error getting semester: ${err.msg}`
-      logger.error(err_msg)
-      res.status(500).send({ message: err_msg })
-    })
-})
-
-router.post('/semester/current', (req, res) => {
-  const { semester } = req.body
-  setCurrentSemester(semester)
-    .then(() => res.status(200).send({ success: true }))
-    .catch((err) => {
-      const err_msg = `Unexpected error change current semester to ${semester}: ${err.msg}`
-      logger.error(err_msg)
-      res.status(500).send({ message: err_msg })
-    })
-})
-
-router.get('/semester/all', (_, res) => {
-  getAllSemesters()
-    .then((data) => res.status(200).send(data))
-    .catch((err) => {
-      const err_msg = `Unexpected error getting all semesters: ${err.message}`
-      logger.error(err_msg)
-      res.status(500).send({ message: err_msg })
-    })
-})
-
-router.get('/semester/survey', (_, res) => {
-  getSurveyStatus()
-    .then((data) => res.status(200).send(data))
-    .catch((err) => {
-      logger.error(`Unexpected error retrieving survey opening: ${err.message}`)
-      res.status(500).send({ message: err.message })
-    })
-})
-
-router.post('/semester/survey', (req, res) => {
-  const semester = req.body
-  setSurveyStatus(semester.surveyOpen)
-    .then((data) => res.status(200).send({ surveyOpen: semester.surveyOpen }))
-    .catch((err) => {
-      logger.error(`Unexpected error retrieving survey opening: ${err.message}`)
-      res.status(500).send({ message: err.message })
     })
 })
 
