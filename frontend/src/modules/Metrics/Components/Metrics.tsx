0d4b8059f76d719a223e42db7e14d095d2ebfea5
Justin Kong <justinkong24@gmail.com>
2024-01-12 20:51:35 -0500

    merged winter 24 changes from main

--- frontend/src/modules/Metrics/Components/Metrics.tsx
+++ frontend/src/modules/Metrics/Components/Metrics.tsx
@@ -298,6 +298,7 @@ export const Metrics = () => {
           <MenuItem value="SP23">Spring 2023</MenuItem>
           <MenuItem value="SU23">Summer 2023</MenuItem>
           <MenuItem value="FA23">Fall 2023</MenuItem>
+          <MenuItem value="WI24:">Winter 2024</MenuItem>
         </DropdownSelect>
       </Box>
       <MetricsTable
