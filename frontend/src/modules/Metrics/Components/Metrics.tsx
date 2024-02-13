2281ed62f4a6e1fa4db21d29b2d0b4ddc20b31b8
Justin Kong <justinkong24@gmail.com>
2024-02-13 11:50:55 -0500

    manually added and fixed metric semesters

--- frontend/src/modules/Metrics/Components/Metrics.tsx
+++ frontend/src/modules/Metrics/Components/Metrics.tsx
@@ -297,7 +297,9 @@ export const Metrics = () => {
           <MenuItem value="WI23">Winter 2023</MenuItem>
           <MenuItem value="SP23">Spring 2023</MenuItem>
           <MenuItem value="SU23">Summer 2023</MenuItem>
-          <MenuItem value="WI24:">Winter 2024</MenuItem>
+          <MenuItem value="FA23">Fall 2023</MenuItem>
+          <MenuItem value="WI24">Winter 2024</MenuItem>
+          <MenuItem value="SP24">Spring 2024</MenuItem>
         </DropdownSelect>
       </Box>
       <MetricsTable
